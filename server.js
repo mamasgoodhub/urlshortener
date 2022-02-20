abfca52c8521b408b4f24c771ee67eff4c9b1310
mamasgoodboy <62749483+mamasgoodhub@users.noreply.github.com>
2022-02-20 22:32:42 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.use('/api/url', require('./routes/api/url'));
 
 const PORT = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV === 'production') {
+//if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "frontend", "build")))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend" , "build", "index.html"));
     });
-}
+//}
 
 
 
