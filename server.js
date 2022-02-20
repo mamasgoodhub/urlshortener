c38d083b4b3dfc08bce7a325be03c610bf606d7b
mamasgoodboy <62749483+mamasgoodhub@users.noreply.github.com>
2022-02-20 23:21:09 +0100

    Test

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 
 //Define Routes
-app.use('/', require('./routes/api/index'));
+//app.use('/', require('./routes/api/index'));
 app.use('/api/url', require('./routes/api/url'));
 
 const PORT = process.env.PORT || 5000;
 
 //if(process.env.NODE_ENV === 'production') {
-app.use(express.static(path.join(__dirname, 'frontend/build')))
+app.use(express.static(path.join(__dirname, "./frontend/build")))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend','build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
 });
 //}
 
