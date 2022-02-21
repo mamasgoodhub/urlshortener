3968162b3c0e823f71599fb672c2f143ad30d03e
mamasgoodboy <62749483+mamasgoodhub@users.noreply.github.com>
2022-02-21 04:28:31 +0100

    Test

--- routes/api/index.js
+++ routes/api/index.js
@@ -18,6 +18,8 @@ router.get('/:code', async (req, res) => {
         const url = await Url.findOne({ urlCode: req.params.code });
 
         if(url) {
+            console.log('Test');
+            console.log(url.longUrl)
             url.timesVisited++
             url.save()
             return res.redirect(url.longUrl);
