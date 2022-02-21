771dceefd398e9edfb8cbf81068b8fb9001f0f98
mamasgoodboy <62749483+mamasgoodhub@users.noreply.github.com>
2022-02-21 05:10:07 +0100

    Test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDB = require('./config/db');
 const path = require('path');
 var cors = require('cors');
 
-const Url = require('/models/Url');
+const Url = require('../models/Url')
 
 const app = express();
 
