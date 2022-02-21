29fe9d7f818563d29036022d7bdb10f97c94d18c
mamasgoodboy <62749483+mamasgoodhub@users.noreply.github.com>
2022-02-21 03:14:13 +0100

    Test

--- frontend/src/context/LinkContext.js
+++ frontend/src/context/LinkContext.js
@@ -1,24 +1,22 @@
 import { createContext, useState, useEffect } from 'react'
 import axios from 'axios';
-const config = require('config');
-require('dotenv').config()
 
 const LinkContext = createContext()
 
 export const LinkProvider = ({children}) => {
     const [link, setLink] = useState([])
-    //const baseUrl = config.get('baseUrl');
+
     useEffect(() => {
         fetchLinks()
     })
 
     const fetchLinks = async () => {
-        axios.get(process.env.baseUrl)
+        axios.get('')
         .then(res =>{
             setLink(res.data)
         })
         .catch(err =>{
-            console.log('Error fetching Links');
+          console.log('Error fetching Links');
         })
       };
 
