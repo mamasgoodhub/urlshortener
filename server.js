f0e72686666f14c94a0c32a0b7dcc48d91d4b872
mamasgoodboy <62749483+mamasgoodhub@users.noreply.github.com>
2022-02-21 04:41:36 +0100

    Test

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 
 //Define Routes
-app.use('/', require('./routes/api/redirect'));
+//app.use('/', require('./routes/api/redirect'));
 app.use('/api/index', require('./routes/api/index'));
 app.use('/api/url', require('./routes/api/url'));
 
